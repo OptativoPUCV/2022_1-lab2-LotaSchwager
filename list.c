175ca9602ccb63f51afcfc17b21694bae3f37c35
LotaSchwager <>
2022-03-20 23:57:27 +0000

    new try

--- list.c
+++ list.c
@@ -88,8 +88,8 @@ void pushFront(List * list, void * data) {
   list->current = list->head;
   list->head = nuevo->data;
 
-  list->head->next = list->current;
-  list->current->prev = list->head;
+  list->head->next = list->current->prev;
+  list->current->prev = list->head->next;
 }
 
 void pushBack(List * list, void * data) {
